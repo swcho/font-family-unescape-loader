c9b1a28d349856a872b73b93675a6803d5e34094
Anthony Fu <anthonyfu117@hotmail.com>
2017-06-27 17:05:06 +0800

    Do not 'unescape' generic font family.

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ const RE_DOUBLE_COUTE = /\\"/g;
 const RE_SPACE = /[ ]+/g;
 const RE_UNICODE = /\\\\([\d\w]{4})/g;
 module.exports = function(source) {
-    // console.log('font-family-unescape-loader');
-    // console.log(source);
+
     source = source.replace(RE_FONT_FAMILY, (str, fontNames) => {
         const strFontNames = fontNames.replace(RE_DOUBLE_COUTE, "'").replace(RE_FONT_NAMES, (match, fontName, sep) => {
-            return `'${fontName.replace(RE_SPACE, ' ').replace(RE_UNICODE, (m, unicode) => String.fromCharCode(parseInt(unicode, 16)))}'${sep || ''}`;
+            if(match.includes("'"))
+                return `'${fontName.replace(RE_SPACE, ' ').replace(RE_UNICODE, (m, unicode) => String.fromCharCode(parseInt(unicode, 16)))}'${sep || ''}`;
+            return match;
         });
         return `font-family: ${strFontNames};`;
     });
-    // console.log(source);
+
     return source;
 }
